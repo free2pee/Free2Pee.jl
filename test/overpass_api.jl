2c427724c49c060fe3267391bc113d6aedeaf4e2
anand jain <anandj@uchicago.edu>
2023-02-20 23:10:58 -0500

    "0.0.0.0"

--- test/overpass_api.jl
+++ test/overpass_api.jl
@@ -1,22 +1,34 @@
 using Free2Pee
 using Oxygen, JSON3, Downloads, CSV, DataFrames, JSONTables, PrettyTables, HTTP, URIs, Distances
+@info "usings"
 
-# lat = 42.3640906
-# lon = -71.1018806
+const LAT = 42.35810736568732
+const LON = -71.10592447014132
 
-# h = HTTP.get(test_uri)
-# 4360543295
-example_query = "https://www.google.com/maps/dir/?api=1&origin=<lat, long>&destination=<lat, long>"
-
+function foo(tag, url)
+    if haskey(tag, :name) 
+        name = tag.name
+        HtmlCell{String}("<a href=\"$url\">$name</a>")
+    else 
+        HtmlCell{String}("<a href=\"$url\">BingBong</a>")
+    end
+end
 
 @get "/{lat}/{lon}" function foo(req::HTTP.Request, lat, lon)
     lat = parse(Float64, lat)
     lon = parse(Float64, lon)
-    df = to_df(lat, lon)
+    df = Free2Pee.to_df(lat, lon)
     view_df = df[:, [:id, :distance, :lat, :lon, :maps_url]]
+    atags = []
+    for r in eachrow(df)
+        atag = foo(r.tags, r.maps_url)
+        push!(atags, atag)
+    end
+
+    view_df.maps_url = atags
     sort!(view_df, :distance)
     io = IOBuffer()
-    pretty_table(io, view_df, nosubheader=true, backend=Val(:html))
+    pretty_table(io, view_df, nosubheader=true, backend=Val(:html); allow_html_in_cells=true)
     
     HTTP.Response(200, ["Content-Type" => "text/html"]; body=take!(io))
 end
@@ -38,8 +50,29 @@ end
     """)
 end
 
-# serve(;host="10.0.0.224")
-serve()
+# CORS headers that show what kinds of complex requests are allowed to API
+headers = [
+    "Access-Control-Allow-Origin" => "*",
+    "Access-Control-Allow-Headers" => "*",
+    "Access-Control-Allow-Methods" => "GET, POST"
+]
+
+function CorsHandler(handle)
+    return function(req::HTTP.Request)
+        # return headers on OPTIONS request
+        if HTTP.method(req) == "OPTIONS"
+            return HTTP.Response(200, headers)
+        else 
+            return handle(req)
+        end
+    end
+end
+
+    # start the web server
+serve(host="0.0.0.0", middleware=[CorsHandler])
+
+    # serve(;host="10.0.0.224")
+# serve(;port=8080)
 
 # try this not haversine
 # curl 'http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,4 @@ using Oxygen, JSON3, Downloads, CSV, DataFrames, JSONTables, PrettyTables, HTTP,
 
 df = Free2Pee.to_df(Free2Pee.LAT, Free2Pee.LON)
 
- 
+"http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false"
