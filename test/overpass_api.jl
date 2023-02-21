708241fb130dd2c3c834ebfa273ddbee1d4715f3
anand jain <anandj@uchicago.edu>
2023-02-21 14:22:20 -0500

    add `radius` queryparam, make `id` atags linking to osm node. make root endpoint the geoloc get + redirect

--- test/overpass_api.jl
+++ test/overpass_api.jl
@@ -6,52 +6,47 @@ const LAT = 42.35810736568732
 const LON = -71.10592447014132
 
 function foo(tag, url)
-    if haskey(tag, :name) 
+    if haskey(tag, :name)
         name = tag.name
         HtmlCell{String}("<a href=\"$url\">$name</a>")
-    else 
+    else
         HtmlCell{String}("<a href=\"$url\">BingBong</a>")
     end
 end
 
 @get "/{lat}/{lon}" function foo(req::HTTP.Request, lat, lon)
+    qps = Oxygen.queryparams(req)
+
+    if haskey(qps, "radius")
+        radius = parse(Int, qps["radius"])
+    else
+        radius = 1000
+    end
+
     lat = parse(Float64, lat)
     lon = parse(Float64, lon)
-    df = Free2Pee.to_df(lat, lon)
-    view_df = df[:, [:id, :distance, :lat, :lon, :maps_url]]
+    df = Free2Pee.to_df(lat, lon; radius)
     atags = []
+    node_atags = map(x->HtmlCell{String}("<a href=\"https://www.openstreetmap.org/node/$x\">$x</a>"), df.id)
+    
     for r in eachrow(df)
         atag = foo(r.tags, r.maps_url)
         push!(atags, atag)
     end
-
+    
+    view_df = df[:, [:id, :distance, :lat, :lon, :maps_url]]
     view_df.maps_url = atags
+    view_df.id = node_atags
+
     sort!(view_df, :distance)
     io = IOBuffer()
     pretty_table(io, view_df, nosubheader=true, backend=Val(:html); allow_html_in_cells=true)
-    
+
     HTTP.Response(200, ["Content-Type" => "text/html"]; body=take!(io))
 end
 
+# "using window.location.replace might not be the best"
 @get "/" function (req::HTTP.Request)
-    dump(req)
-    HTTP.Response(200, ["Content-Type" => "text/html"]; body="""
-    <html>
-    <head>
-    <title>Free2Pee</title>
-    </head>
-    <body>
-    <h1>Free2Pee</h1>
-    <p>Find the nearest public toilets</p>
-    <form action="/$LAT/$LON">
-    <input type="submit" value="Submit">
-    </form>
-    </body>
-    </html>
-    """)
-end
-
-@get "/loc" function (req::HTTP.Request)
     HTTP.Response(200, ["Content-Type" => "text/html"]; body="""
 <!DOCTYPE html>
 <html>
@@ -84,8 +79,7 @@ function getLocation() {
 
 function showPosition(position) {
     href = "/" + position.coords.latitude + "/" + position.coords.longitude
-    s = `<a href=\"\${href}">BINGBONG</a>`
-    x.innerHTML=s;
+    window.location.replace(href);
 }
 </script>
 
@@ -102,21 +96,21 @@ headers = [
 ]
 
 function CorsHandler(handle)
-    return function(req::HTTP.Request)
+    return function (req::HTTP.Request)
         # return headers on OPTIONS request
         if HTTP.method(req) == "OPTIONS"
             return HTTP.Response(200, headers)
-        else 
+        else
             return handle(req)
         end
     end
 end
 
-    # start the web server
+# start the web server
 # serve(host="0.0.0.0", middleware=[CorsHandler])
-serve(;middleware=[CorsHandler])
+serve(; middleware=[CorsHandler])
 
-    # serve(;host="10.0.0.224")
+# serve(;host="10.0.0.224")
 # serve(;port=8080)
 
 # try this not haversine
